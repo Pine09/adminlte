31871001005d20cfd3edd8c7cf82f850b2eded12
pine09 <ronaldo_n09@yahoo.com>
2017-02-26 23:43:00 +0700

    1st commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+		DB::table('users')->insert([
+            'name' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+		 factory(App\User::class, 50)->create()->each(function ($u) {
+        $u->posts()->save(factory(App\Post::class)->make());
+    });
+    }
+}
